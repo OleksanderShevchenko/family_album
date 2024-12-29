fabb795bf6efc789879bb1df9baf3d3a2324fe54
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2024-12-29 19:36:36 +0200

    Extend test expected data for new image

--- tests/test_image_utils.py
+++ tests/test_image_utils.py
@@ -13,15 +13,18 @@ class TestImageUtils(unittest.TestCase):
         self._image_sizes = {'test_photo.jpg': (1600, 720),
                              'test_photo2.jpg': (720, 1600),
                              'test_photo3.jpg': (4000, 1800),
-                             'test_photo4.jpg': (4000, 1800)}
+                             'test_photo4.jpg': (4000, 1800),
+                             'test.jpg': (400, 300)}
         self._image_take_date = {'test_photo.jpg': pd.NaT,
                                  'test_photo2.jpg': pd.NaT,
                                  'test_photo3.jpg': datetime(year=2024, month=2, day=23, hour=11, minute=24, second=48),
-                                 'test_photo4.jpg': datetime(year=2024, month=2, day=20, hour=11, minute=42, second=58)}
+                                 'test_photo4.jpg': datetime(year=2024, month=2, day=20, hour=11, minute=42, second=58),
+                                 'test.jpg': datetime(year=2010, month=1, day=1, hour=0, minute=00, second=00)}
         self._exif_size = {'test_photo.jpg': 0,
                            'test_photo2.jpg': 0,
                            'test_photo3.jpg': 58,
-                           'test_photo4.jpg': 58}
+                           'test_photo4.jpg': 58,
+                           'test.jpg': 56}
 
     def test_is_image(self):
         for dir_name, _, files in os.walk(self._data_path):

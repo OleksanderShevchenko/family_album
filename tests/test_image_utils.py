2b6fa28578f012df87873bd4fe269ff67e4a8277
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2024-12-29 14:35:49 +0200

    Correct assert to make it workable. The assertion for equality for NaT constants is inconsistent.

--- tests/test_image_utils.py
+++ tests/test_image_utils.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import os.path
 import unittest
 from datetime import datetime
@@ -6,20 +7,21 @@ from src.utility_functions.image_utils import (is_image_file, get_image_size, ge
 
 
 class TestImageUtils(unittest.TestCase):
+
     def setUp(self):
         self._data_path = os.path.abspath('./data/images/')
         self._image_sizes = {'test_photo.jpg': (1600, 720),
                              'test_photo2.jpg': (720, 1600),
                              'test_photo3.jpg': (4000, 1800),
                              'test_photo4.jpg': (4000, 1800)}
-        self._image_take_date = {'test_photo.jpg': None,
-                                 'test_photo2.jpg': None,
+        self._image_take_date = {'test_photo.jpg': pd.NaT,
+                                 'test_photo2.jpg': pd.NaT,
                                  'test_photo3.jpg': datetime(year=2024, month=2, day=23, hour=11, minute=24, second=48),
                                  'test_photo4.jpg': datetime(year=2024, month=2, day=20, hour=11, minute=42, second=58)}
         self._exif_size = {'test_photo.jpg': 0,
-                          'test_photo2.jpg': 0,
-                          'test_photo3.jpg': 58,
-                          'test_photo4.jpg': 58}
+                           'test_photo2.jpg': 0,
+                           'test_photo3.jpg': 58,
+                           'test_photo4.jpg': 58}
 
     def test_is_image(self):
         for dir_name, _, files in os.walk(self._data_path):
@@ -39,7 +41,11 @@ class TestImageUtils(unittest.TestCase):
                 full_name = os.path.join(dir_name, file)
                 take_date = get_image_creation_date(full_name)
                 expected_date = self._image_take_date[file]
-                self.assertEqual(expected_date, take_date)
+                if take_date is pd.NaT:
+                    self.assertTrue(expected_date is take_date)
+                else:
+                    self.assertEqual(expected_date, take_date)
+
 
     def test_image_exif_data(self):
         for dir_name, _, files in os.walk(self._data_path):

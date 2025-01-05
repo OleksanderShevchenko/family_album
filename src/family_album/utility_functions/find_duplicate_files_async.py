1cf621332a9224adc7a074d2bdc4f40b368a4db5
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-01-05 22:35:59 +0200

    Refactor module find_duplicate_files_async.py:  - use asynchronous read of the file to calculate hash;  - rename function hash_file to _get_file_hash;  - add type hints;  - rename variables to better descriptive;  - translate comments;  - add error trapping in _get_file_hash;   - improve testing run of the module and compare with synchronous version

--- src/family_album/utility_functions/find_duplicate_files_async.py
+++ src/family_album/utility_functions/find_duplicate_files_async.py
@@ -3,50 +3,84 @@ import hashlib
 import json
 import os
 from time import perf_counter
+from typing import List, Dict, Tuple
+
+import aiofiles
+
+_BLOCK_SIZE = 65536
+_NUM_OPEN_FILES = 200
 
 
-async def hash_file(path) -> str:
+async def _get_file_hash(file_full_name: str, semaphore: asyncio.Semaphore) -> Tuple[str, str]:
     """
-    Асинхронно обчислює хеш вмісту файлу за вказаним шляхом.
+    Calculate hash of the file asynchronously.
     """
-    BLOCKSIZE = 65536
+
     hasher = hashlib.blake2b()
-    with open(path, 'rb') as file:
-        buf = file.read(BLOCKSIZE)
-        while len(buf) > 0:
-            hasher.update(buf)
-            buf = file.read(BLOCKSIZE)
-    return hasher.hexdigest()
+    try:
+        async with semaphore:
+            async with aiofiles.open(file_full_name, 'rb') as file:
+                while True:
+                    buf = await file.read(_BLOCK_SIZE)
+                    if not buf:
+                        break
+                    hasher.update(buf)
+    except Exception as e:
+        print(f"Error reading file {file_full_name}: {e}")
+        return "", file_full_name
+    return hasher.hexdigest(), file_full_name
 
-async def find_duplicate_files_async(root_folder) -> dict:
+
+async def find_duplicate_files_async(root_folder: str) -> Dict[str, List[str]]:
     """
-    Асинхронна функція для пошуку дублікатів файлів в директорії та її піддиректоріях.
-    Повертає словник, де ключ - це оригінальний файл, а значеннями - список файлів, які його дублюють.
+    Asynchronously analyze files in a directory (and subdirectories) to find duplicated files.
+    Files are considered duplicates if they have the same hash code.
+    Returns a dictionary where the key is the hash code and the value is a list of file paths with that hash.
     """
     file_dict = {}
     tasks = []
-    for dirpath, dirnames, filenames in os.walk(root_folder):
+    semaphore = asyncio.Semaphore(_NUM_OPEN_FILES)  # Limit the number of concurrent file operations
+    for directory, _, filenames in os.walk(root_folder):
         for filename in filenames:
-            path = os.path.join(dirpath, filename)
-            task = asyncio.ensure_future(hash_file(path))
-            tasks.append((path, task))
-    for path, task in tasks:
-        file_hash = await task
-        if file_hash in file_dict:
-            file_dict[file_hash].append(path)
-        else:
-            file_dict[file_hash] = [path]
+            file_path = os.path.join(os.fsdecode(directory), os.fsdecode(filename))
+            tasks.append(_get_file_hash(file_path, semaphore))
+
+    results = await asyncio.gather(*tasks)  # rau tasks
+    for file_hash, file_full_name in results:
+        if file_hash:
+            if file_hash in file_dict.keys():
+                if file_full_name not in file_dict[file_hash]:  # avoid duplicating same file
+                    file_dict[file_hash].append(file_full_name)
+            else:
+                file_dict[file_hash] = [file_full_name]
+
     return file_dict
 
 
 if __name__ == "__main__":
-    path = "/home/oleksander/Family Album/Photo and video from Cloud"  # on the test flash card with 103 GB takes ~45 min
+    testing_directory = "<put your dir here>"  # dir to check for duplication
+    save_result_directory = "<put your dir here>"   # dir to save results
     t1_start = perf_counter()
-    duplicates = asyncio.run(find_duplicate_files_async(path))
+    duplicates = asyncio.run(find_duplicate_files_async(testing_directory))
     t1_stop = perf_counter()
-    print(f' Checking dir{path} for duplicate files takes {t1_stop - t1_start} seconds.')
-    print(f'Duplicates are: {duplicates}')
-    print(f' Checking dir{path} for duplicate files takes {t1_stop - t1_start} seconds.')
-    with open(r"C:\Users\Oleksander\duplicates_async.txt", 'wt') as fp:
+    duplications_only = {file[0]: file[1:] for _, file in duplicates.items()
+                         if len(file) > 1}
+    print(f'Async function was checking dir "{testing_directory}" for duplicate files for {t1_stop - t1_start} seconds.')
+
+    with open(os.path.join(save_result_directory, "duplicates_async.txt"), 'wt') as fp:
+        fp.write(json.dumps(duplications_only))
+    with open(os.path.join(save_result_directory,"hash_files_async.txt"), 'wt') as fp:
+        fp.write(json.dumps(duplicates))
+
+    from src.family_album.utility_functions.find_duplicate_files import find_duplicate_files
+
+    t1_start = perf_counter()
+    duplicates = find_duplicate_files(testing_directory)
+    t1_stop = perf_counter()
+    print(f'Synchronous function was checking dir "{testing_directory}" for duplicate files for {t1_stop - t1_start} seconds.')
+
+    with open(os.path.join(save_result_directory, "duplicates_sync.txt"), 'wt') as fp:
+        fp.write(json.dumps(duplications_only))
+    with open(os.path.join(save_result_directory, "hash_files_sync.txt"), 'wt') as fp:
         fp.write(json.dumps(duplicates))
     print("Done!")

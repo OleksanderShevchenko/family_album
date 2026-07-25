288f49318de33e36c4c0def47eb3c40426ccb62c
Oleksander Shevchenko <oleksander.shevchenko777@gmail.com>
2026-07-25 22:02:32 +0300

    Remove _NUM_OPEN_FILES constant and do not check instantly_opened_files argument - trust to value passed. The constant and check will be moved to wrapper class.

--- src/family_album_lib/duplicate_file_analyser.py
+++ src/family_album_lib/duplicate_file_analyser.py
@@ -10,18 +10,13 @@ from src.family_album_lib.directory_analyser import DirectoryAnalyser
 
 class DuplicateFileAnalyser():
 
-    _NUM_OPEN_FILES = 200
-
     def __init__(self, directory: str, instantly_opened_files: int = 0) -> None:
         super().__init__()
         self._directory_analyser: DirectoryAnalyser = DirectoryAnalyser(directory)
         self.__files_hashes: Dict[str, List[str]] = {}
         self.__files_analysed: int = 0
         self.__progress: int = 0
-        if instantly_opened_files <= 0:
-            self.__num_of_threads = self._NUM_OPEN_FILES
-        else:
-            self.__num_of_threads = instantly_opened_files
+        self.__num_of_threads = instantly_opened_files
         self.start_analysis: Callable = None
         self.update_progress: Callable = None
         self.log_event: Callable = None
